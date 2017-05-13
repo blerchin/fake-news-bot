8bb8d0a8f7330242edc32988dccb918e0a86921f
Ben Lerchin <ben@lerchin.com>
2017-05-13 14:46:00 -0700

    add text-to-spech

--- static/js/app.js
+++ static/js/app.js
@@ -9,10 +9,26 @@ var outbox = new ReconnectingWebSocket(ws_scheme + location.host + '/submit')
 
 var tweetEl = document.getElementById('tweet');
 
+var voice = null;
+if (window.speechSynthesis) {
+  setTimeout(function(){
+    voices = speechSynthesis.getVoices();
+    voices.forEach(function(v) {
+      if(!voice && v.name == 'Alex') {
+        //fall back to Alex
+        voice = v;
+      } else if(v.name == 'Google US English') {
+        // prefer Google US
+        //voice = v;
+      }
+    });
+  }, 1000)
+}
+
 inbox.onmessage = function(message) {
   var data = JSON.parse(message.data);
   if (data.evt == 'new:tweet') {
-    tweetEl.textContent = data.tweet;
+    updateTweet(data.tweet);
   } else {
     console.log(data);
   }
@@ -33,3 +49,48 @@ document.addEventListener('keydown', function(e) {
     outbox.send(JSON.stringify({ evt: 'button:pressed' }))
   }
 });
+
+function updateTweet(text) {
+    if (!speechSynthesis || !speechSynthesis.speaking) {
+      var tweet = stripUrls(htmlDecode(text));
+      tweetEl.textContent = tweet;
+      speakTweet(formatHandles(formatHashtags(tweet)));
+    }
+}
+
+function speakTweet(text) {
+  if(!voice) { return; }
+  var msg = new SpeechSynthesisUtterance();
+  msg.voice = voice;
+  msg.text = text;
+  speechSynthesis.speak(msg);
+}
+
+function formatHandles(text) {
+  return text.replace(/@([A-Za-z0-9_].*?)/g, function(match, p1) {
+    return '; at ' + toCapitalizedWords(p1);
+  });
+}
+
+function formatHashtags(text) {
+  //format hashtags for reading
+  return text.replace(/#(.*?)[^A-Za-z0-9]/g, ' hashtag $1 ');
+}
+
+function stripUrls(text){
+  return text.replace(/https:\/\/(.*?)[^A-Za-z0-9.\/]/g, ' ');
+}
+
+function htmlDecode(input) {
+  var doc = new DOMParser().parseFromString(input, "text/html");
+  return doc.documentElement.textContent;
+}
+function toCapitalizedWords(name) {
+    var words = name.match(/[A-Za-z][a-z]*/g);
+
+    return words.map(capitalize).join(" ");
+}
+
+function capitalize(word) {
+    return word.charAt(0).toUpperCase() + word.substring(1);
+}
