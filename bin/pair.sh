081cbff17e661d4940ace9d3d3bb249b5e2edfc5
Ben Lerchin <ben@lerchin.com>
2017-05-14 06:01:26 +0000

    add bluetooth/ttl scripts

---
+++ bin/pair.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+BT_MAC="FC:58:FA:1A:90:92"
+
+pulseaudio -D
+
+expect << EOF
+spawn "bluetoothctl"
+expect "# "
+send "power on\r"
+expect "# "
+send "agent on\r"
+expect "Agent registered"
+send "default-agent\r"
+expect "Default agent request successful"
+send "scan on\r"
+expect "Discovery started"
+expect "Device ${BT_MAC}" 
+send "pair ${BT_MAC}\r"
+expect "Pairing successful"
+send "trust ${BT_MAC}\r"
+expect "trust succeeded"
+send "connect ${BT_MAC}\r"
+expect "Connection successful"
+send "exit"
+EOF
+
+pacmd set-default-sink bluez_sink.FC_58_FA_1A_90_92
+
+pico2wave -w ttl.wav "$1" && aplay ttl.wav
