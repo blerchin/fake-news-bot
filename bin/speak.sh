d3eae149ca72bb868188dec01e65583e052e4387
Ben Lerchin <ben@lerchin.com>
2017-05-15 21:44:56 +0000

    use alsaplayer, handle failed connection

--- bin/speak.sh
+++ bin/speak.sh
@@ -5,6 +5,7 @@ export BT_MAC="FC:58:FA:1A:90:92"
 pulseaudio -D
 
 expect << EOF
+set timeout 30
 spawn "bluetoothctl"
 expect "# "
 send "power on\r"
@@ -14,11 +15,14 @@ expect "Agent registered"
 send "default-agent\r"
 expect "Default agent request successful"
 send "connect ${BT_MAC}\r"
-expect "Connection successful"
-send "exit"
+expect {
+	"Connection successful" { send "exit" }
+	"Failed to connect: " { send "exit" }
+}
 EOF
 
 pacmd set-default-sink "bluez_sink.$(echo $BT_MAC | tr : _)"
+pactl set-sink-volume "bluez_sink.$(echo $BT_MAC | tr : _)" 80%
 
 
-pico2wave -w ttl.wav "$1" && aplay ttl.wav
+pico2wave -w ttl.wav "$1" && alsaplayer ttl.wav 
