78b6b6bdcdb33f6e426e29337063b7acee6655ae
Ben Lerchin <ben@lerchin.com>
2017-05-16 20:53:22 +0000

    which chromium-browser

--- bin/browser.sh
+++ bin/browser.sh
@@ -1,1 +1,1 @@
-startx /usr/bin/chromium-browser --app=https://fake-news-bot.herokuapp.com --window-position=0,0 --window-size=1824,984
+startx `which chromium-browser` --app=https://fake-news-bot.herokuapp.com --window-position=0,0 --window-size=1824,984
